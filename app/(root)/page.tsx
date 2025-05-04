392d09e6f5a8fdcd7e8a4d084fa825d7afa978b7
Wanderson Kenedy <42207590+wandskk@users.noreply.github.com>
2025-05-04 18:51:36 -0300

    feat(products): add sample products and product list component
    Created static sample product data and a product list UI component to render them.
    This sets the foundation for future dynamic integration with backend or CMS.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,8 @@
+import ProductList from "@/components/shared/product/productList";
+import sampleData from "@/db/sample-data";
+
 const Homepage = async () => {
-  return <>DevwkStore</>;
+  return <ProductList data={sampleData.products} title="Newst Arrivals" limit={4} />;
 };
 
 export default Homepage;
