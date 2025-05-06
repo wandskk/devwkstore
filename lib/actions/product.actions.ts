fd32a135311f2dfbfdbe6d17972abc155ed3ff81
Wanderson Kenedy <42207590+wandskk@users.noreply.github.com>
2025-05-06 13:41:47 -0300

    chore(config): update environment setup for serverless deployment
    Configured environment variables for serverless deployment.
    Ensured compatibility with platforms like Vercel by adjusting .env handling and Prisma client settings.
    Prepared the project for seamless serverless execution.

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,10 +1,8 @@
 'use server';
-import { PrismaClient } from "@/lib/generated/prisma";
+import { prisma } from "@/db/prisma"
 import { LATEST_PRODUCTS_LIMIT } from "@/lib/constants/index"
 
 export async function getLatestProducts() {
-    const prisma = new PrismaClient();
-
     const data = await prisma.product.findMany({
         take: LATEST_PRODUCTS_LIMIT,
         orderBy: {
