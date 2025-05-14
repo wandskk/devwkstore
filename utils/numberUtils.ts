50927620413435edf56141b9d4aebf5dbb67673b
Wanderson Kenedy <42207590+wandskk@users.noreply.github.com>
2025-05-14 13:39:53 -0300

    feat(cart): calculate item price and save to database
    Implemented price calculation logic before inserting item into the cart.
    Stored calculated price in the database using Prisma.

--- utils/numberUtils.ts
+++ utils/numberUtils.ts
@@ -1,6 +1,14 @@
 export const numberUtils = {
-    formatNumberWithDecimal: (num: number): string => {
-        const [int, decimal] = num.toString().split('.')
-        return decimal ? `${int}.${decimal.padEnd(2, '0')}` : `${int}.00`
-    }
+  formatNumberWithDecimal: (num: number): string => {
+    const [int, decimal] = num.toString().split(".");
+    return decimal ? `${int}.${decimal.padEnd(2, "0")}` : `${int}.00`;
+  },
+  roundTwoDecimals: (value: number | string): number => {
+    const num = Number(value);
+
+    if (isNaN(num))
+      throw new Error("Value is not a valid number or numeric string");
+
+    return Math.round((num + Number.EPSILON) * 100) / 100;
+  },
 };
