567dbce4016c99d078b9317725c91c0bd83e02ce
Wanderson Kenedy <42207590+wandskk@users.noreply.github.com>
2025-05-16 02:25:17 -0300

    feat(checkout): add Zod schema and action for payment method selection
    Created a Zod schema to validate the payment method.
    Implemented a server action to store the selected method in the database.
    Integrated logic to persist data during checkout flow.

---
+++ app/(root)/payment-method/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PaymentMethodPage = () => {
+  return <div>Payme</div>;
+};
+
+export default PaymentMethodPage;
---
+++ lib/validators/paymentMethod.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+import { PAYMENT_METHODS } from "../constants";
+
+export const paymentMethodSchema = z
+  .object({
+    type: z.string().min(1, "Payment method is required"),
+  })
+  .refine((data) => PAYMENT_METHODS.includes(data.type), {
+    path: ["type"],
+    message: "Invalid payment method",
+  });
