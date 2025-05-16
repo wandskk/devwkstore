38c1a417ba52f5120907783faa4fad820917df34
Wanderson Kenedy <42207590+wandskk@users.noreply.github.com>
2025-05-16 01:32:59 -0300

    feat(cart): persist session cart across user sessions
    Implemented logic to persist the cart using session cookies.
    Ensures that users retain cart contents even after page reloads or temporary navigation.

--- auth.ts
+++ auth.ts
@@ -6,6 +6,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { compareSync } from "bcrypt-ts-edge";
 import type { NextAuthConfig } from "next-auth";
 import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
 export const config = {
   pages: {
@@ -63,8 +64,9 @@ export const config = {
 
       return session;
     },
-    async jwt({ token, user }: any) {
+    async jwt({ token, user, trigger, session }: any) {
       if (user) {
+        token.id = user.id;
         token.role = user.role;
 
         if (user.name === "NO_NAME") {
@@ -76,6 +78,31 @@ export const config = {
           });
         }
       }
+
+      if (trigger === 'signIn' || trigger === 'signUp') {
+        const cookiesObject = await cookies();
+        const sessionCartId = cookiesObject.get('sessionCartId')?.value;
+
+        if (sessionCartId) {
+          const sessionCart = await prisma.cart.findFirst({
+            where: { sessionCartId }
+          })
+
+          if (sessionCart) {
+            await prisma.cart.deleteMany({
+              where: { userId: user.id }
+            })
+
+            await prisma.cart.update({
+              where: { id: sessionCart.id },
+              data: {
+                userId: user.id,
+              }
+            })
+          }
+        }
+      }
+
       return token;
     },
     authorized({ request, auth }: any) {
