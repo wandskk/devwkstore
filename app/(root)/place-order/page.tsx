a5434e3369703020467d0921c7730a5cf46d88a9
Wanderson Kenedy <42207590+wandskk@users.noreply.github.com>
2025-05-18 07:22:42 -0300

    feat(order): create place order page with UI and logic
    Implemented the Place Order page to review order details and confirm purchase.
    Integrated with order state and displayed cart summary, shipping, and payment method.

--- app/(root)/place-order/page.tsx
+++ app/(root)/place-order/page.tsx
@@ -8,16 +8,7 @@ import { ShippingAddress } from "@/types/shippingAddress";
 import { Card, CardContent } from "@/components/ui/card";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import Image from "next/image";
-import CartTable, { RenderQtyButtonsActions } from "../cart/cartTable";
+import CartTable from "../cart/cartTable";
 import { currencyUtils } from "@/utils/currencyUtils";
 
 export const metadata: Metadata = {
@@ -25,6 +16,8 @@ export const metadata: Metadata = {
 };
 
 const PlaceOrderPage = async () => {
+  
+
   const cart = await getMyCart();
   const session = await auth();
   const userId = session?.user?.id;
